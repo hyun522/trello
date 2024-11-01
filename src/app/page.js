0c651b728f7fc6640742373d2943d1538c01b5b8
hyun522 <jhj1004@naver.com>
2024-11-02 01:58:28 +0900

    FEAT : main. 컴포넌트 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Header from '@/components/common/Header';
+import Main from '@/components/common/Main';
 import '@/styles/globals.css';
 export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
 }
---
+++ src/components/common/Main.js
@@ -0,0 +1,18 @@
+export default function Main() {
+  return (
+    <div className='h-screen flex justify-center items-center text-center'>
+      <div>
+        <h2 className='text-[24px] font-bold'>Trello</h2>
+        <p className='max-w-[672px] mt-[10px]'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          possimus magnam tempore ipsum numquam perspiciatis laboriosam ad odit
+          ullam nostrum ex ducimus earum iste repellat accusamus distinctio
+          iure, quibusdam dolores?
+        </p>
+        <button className='max-w-[250px] mt-[10px] bg-black text-white p-[5px] rounded-[6px]'>
+          Get Trello Clone For Free
+        </button>
+      </div>
+    </div>
+  );
+}
