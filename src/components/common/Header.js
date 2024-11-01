a3a8b69a0be5bb3a6a9c9bc903abfba52d43b82f
hyun522 <jhj1004@naver.com>
2024-11-02 02:21:58 +0900

    DESIGN : 반응형 적용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,22 +2,22 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center p-[10px] border-[1px]'>
-      <div className='flex'>
+    <div className='md:flex justify-between items-center p-[10px] border-[1px] '>
+      <div className='hidden md:flex'>
         <Image
           src='/images/trello-logo.png'
           width={25}
           height={25}
           alt='로고이미지'
-          className='mr-[3px]'
+          className='md:mr-[3px]'
         />
         <p>Trello</p>
       </div>
-      <div>
-        <button className='border border-[1px] rounded-[6px] p-[5px] mr-[10px]'>
+      <div className='flex justify-between'>
+        <button className='md:border border-[1px] rounded-[6px] p-[5px] mr-[10px]'>
           Login
         </button>
-        <button className='rounded-[6px] p-[5px] bg-black text-white'>
+        <button className='md: rounded-[6px] p-[5px] bg-black text-white'>
           Sign up
         </button>
       </div>
