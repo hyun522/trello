fc64813606ef076097ac07cb776f3843bf20f39a
hyun522 <jhj1004@naver.com>
2024-11-03 14:42:26 +0900

    Revert "FEAT : signUp 페이지 구현"
    This reverts commit ceb1f576ee87cf65b5c6342fa3dea7ef48819887.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,8 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className='md:flex justify-between items-center p-[10px] border-[1px]'>
+    <div className='md:flex justify-between items-center p-[10px] border-[1px] '>
       <div className='hidden md:flex'>
         <Image
           src='/images/trello-logo.png'
@@ -18,11 +17,9 @@ export default function Header() {
         <button className='md:border border-[1px] rounded-[6px] p-[5px] mr-[10px]'>
           Login
         </button>
-        <Link href='/signup'>
-          <button className='md: rounded-[6px] p-[5px] bg-black text-white'>
-            Sign up
-          </button>
-        </Link>
+        <button className='md: rounded-[6px] p-[5px] bg-black text-white'>
+          Sign up
+        </button>
       </div>
     </div>
   );
